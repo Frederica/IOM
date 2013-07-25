95d6a65edc9ede29aef09ac47888f44404df5c73
Frederica <frederica.stahl@gmail.com>
2013-07-25 17:16:00 -0400

    new draft

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -3,4 +3,7 @@ class VisitorsController < ApplicationController
   def new
   end
 
+  def intro
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 IOM::Application.routes.draw do
   root :to => 'visitors#new'
+  get '/intro' , :to => 'visitors#intro', :as => 'intro'
 end

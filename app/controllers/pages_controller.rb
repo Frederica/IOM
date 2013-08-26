2bd45b8f54705564afd92c4f0590e3a94241a333
Frederica <frederica.stahl@gmail.com>
2013-08-25 21:10:04 -0400

    first draft of second version

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,14 @@
+class PagesController < ApplicationController
+
+  def story
+  end
+  def cause
+  end
+  def cast
+  end
+  def content
+  end
+  def engage
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 IOM::Application.routes.draw do
-  root :to => 'visitors#new'
+  root :to => 'visitors#home'
+
+  get 'story', :to => "pages#story", :as => "story"
+  get 'cast', :to => "pages#cast", :as => "cast"
+  get 'cause', :to => "pages#cause", :as => "cause"
+  get 'content', :to => "pages#content", :as => "content"
+  get 'engage', :to => "pages#engage", :as => "engage"
+
+ # change to 'visitors#new' for old homepage
 end
